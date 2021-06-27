1d6229c3b57d1e50aa977cb6ccfac0133908158d
Sudalaisivam <85403756+Sudalaisivam@users.noreply.github.com>
2021-06-27 21:32:08 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        {" "}
+        A basic Web Development React Js bootcampThis was an amazing bootcamp
+        taken up by Shuarya Sinha. We covered everything from scratch including
+        Javascript,React.js,HTML.
+      </p>
+    </div>
+  );
+}
+export default Info;
